674efcc8baee0a2367daa1a23d2b132eaa18a773
David Asensio Ortega <dasensio13@gmail.com>
2022-07-08 12:09:20 +0200

    add some custom queries

--- src/main/java/org/transicion/portillo/persistence/repository/AssociateRepository.java
+++ src/main/java/org/transicion/portillo/persistence/repository/AssociateRepository.java
@@ -1,7 +1,15 @@
 package org.transicion.portillo.persistence.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.transicion.portillo.persistence.model.Associate;
 
 public interface AssociateRepository extends JpaRepository<Associate, Long> {
+
+	List<Associate> findByName(String name);
+
+	List<Associate> findByDateCreatedBetween(LocalDate start, LocalDate end);
+
 }
--- src/test/java/org/transicion/portillo/persistence/repository/AssociateRepositoryTests.java
+++ src/test/java/org/transicion/portillo/persistence/repository/AssociateRepositoryTests.java
@@ -1,6 +1,7 @@
 package org.transicion.portillo.persistence.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,9 +23,28 @@ class AssociateRepositoryTests {
 	}
 
 	@Test
-	void findAssociate() {
+	void findAssociateById_NotFound() {
 		Associate findAssociate = repository.findById(1234L).orElse(null);
 		Assertions.assertNull(findAssociate);
+	}
+
+	@Test
+	void findAssociateByName_Found() {
+		repository.save(new Associate(2L, "test2", "test@mail.com", LocalDate.now(), true));
+
+		List<Associate> findAssociates = repository.findByName("test2");
+		Assertions.assertEquals(1, findAssociates.size());
+		Assertions.assertEquals(2L, findAssociates.get(0).getId());
+	}
+
+	@Test
+	void findAssociateByDate_Found() {
+		repository.save(new Associate(3L, "test3", "test@mail.com", LocalDate.now().minusDays(2), true));
+
+		List<Associate> findAssociates = repository.findByDateCreatedBetween(LocalDate.now().minusDays(3),
+				LocalDate.now().minusDays(1));
+		Assertions.assertEquals(1, findAssociates.size());
+		Assertions.assertEquals(3L, findAssociates.get(0).getId());
 	}
 
 }
